004692fdfd3e20b665bc48f5d470ae96418f1740
Kim Jae Keun <85093525+klolarion@users.noreply.github.com>
2025-02-23 22:30:15 +0900

    Update HealthCheckController.java

--- backend/core/src/main/java/com/bombi/core/presentation/controller/HealthCheckController.java
+++ backend/core/src/main/java/com/bombi/core/presentation/controller/HealthCheckController.java
@@ -1,15 +1,16 @@
 package com.bombi.core.presentation.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/core")
+@RequestMapping("/core/health")
 public class HealthCheckController {
 
-    @GetMapping("/health")
-    public String healthCheck() {
-        return "Core :: Healthy";
+    @GetMapping
+    public ResponseEntity<?> coreHealth() {
+        return ResponseEntity.ok("Core :: Healthy");
     }
 }
