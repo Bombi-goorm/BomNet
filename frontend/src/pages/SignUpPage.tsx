a59a5fd3b9f1ff4d98544188137ac787412756e0
Klolarion <85093525+klolarion@users.noreply.github.com>
2025-03-19 09:28:37 +0900

    fix/signup-build-JK
    빌드오류수정

--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Header from "../components/Header";
+// import Header from "../components/Header";
 import { useNavigate } from "react-router-dom";
 import { signup } from "../api/core_api";
 
