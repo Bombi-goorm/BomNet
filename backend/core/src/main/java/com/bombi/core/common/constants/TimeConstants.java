4b6c2a59c61985f991980718f98d2c346aaefbb1
ostar11 <cjw5877@gmail.com>
2025-04-16 23:23:22 +0900

    JW-cache/refactor : 시간 문자열 클래스 생성

---
+++ backend/core/src/main/java/com/bombi/core/common/constants/TimeConstants.java
@@ -0,0 +1,7 @@
+package com.bombi.core.common.constants;
+
+public class TimeConstants {
+
+	public static final String LOCAL_DATE_TIME_STRING = "yyyy-MM-dd HH:mm:ss";
+	public static final String LOCAL_DATE_STRING = "yyyy-MM-dd";
+}
