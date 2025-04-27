ae296ef849e1e8d836ba3158e57246cd19ead3a1
ostar11 <cjw5877@gmail.com>
2025-04-28 00:58:11 +0900

    JW-cache/refactor : QualityItemPriceService timeStringProvider로 시간 문자열 변경

--- backend/core/src/main/java/com/bombi/core/application/service/price/QualityItemPriceService.java
+++ backend/core/src/main/java/com/bombi/core/application/service/price/QualityItemPriceService.java
@@ -6,24 +6,25 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.bombi.core.common.utils.time.TimePolicy;
+import com.bombi.core.common.utils.time.provider.TimeStringProvider;
 import com.bombi.core.infrastructure.external.price.variety.client.QualityVarietyPriceCollector;
 import com.bombi.core.presentation.dto.price.QualityChartData;
 
 @Service
 public class QualityItemPriceService {
 
-	private final TimePolicy timePolicy;
+	private final TimeStringProvider timeStringProvider;
 	private final QualityVarietyPriceCollector qualityVarietyPriceCollector;
 
-	public QualityItemPriceService(@Qualifier("dailyPriceTimePolicy") TimePolicy timePolicy,
+	public QualityItemPriceService(@Qualifier("dailyTimeStringProvider") TimeStringProvider timeStringProvider,
 		QualityVarietyPriceCollector qualityVarietyPriceCollector) {
-		this.timePolicy = timePolicy;
+		this.timeStringProvider = timeStringProvider;
 		this.qualityVarietyPriceCollector = qualityVarietyPriceCollector;
 	}
 
 	public List<QualityChartData> getQualityItemPrice(String item) {
-		String startDateTime = timePolicy.getStartTime();
-		String endDateTime = timePolicy.getEndTime();
+		String startDateTime = timeStringProvider.getStartDateString();
+		String endDateTime = timeStringProvider.getEndDateString();
 
 		return qualityVarietyPriceCollector.sendVarietyPriceTrend(item, startDateTime, endDateTime);
 	}
